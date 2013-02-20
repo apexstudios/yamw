660ff4f31066e197fa664a66d4371f18f7a59756
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-02-20 01:04:51 +0100

    moved index.php into webroot.php

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -18,11 +18,11 @@ $start_time = microtime(true);
 global $num_queries;
 $num_queries = 0;
 
-require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/inc.php';
+require_once __DIR__.'/../../vendor/autoload.php';
+require_once __DIR__.'/../inc.php';
 
-require_once __DIR__.'/Yamw/Lib/Constants.php';
-require_once __DIR__.'/Yamw/Lib/Settings.php';
+require_once __DIR__.'/../Yamw/Lib/Constants.php';
+require_once __DIR__.'/../Yamw/Lib/Settings.php';
 
 useHelper('Template');
 useHelper('Forward');
