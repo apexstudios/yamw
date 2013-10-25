4e5ace470c4003792ee7d3a833cfe2c7594e5d4d
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-10-25 23:41:38 +0200

    Fixing request tests

--- Tests/RequestTest.php
+++ Tests/RequestTest.php
@@ -7,66 +7,70 @@ class RequestTest extends PHPUnit_Framework_TestCase
 
     public function testInitializationPopulatesInitialState()
     {
+        $request = new Request;
         // Just to be sure that it is non-existent
-        self::assertSame(null, Request::get('heyho'));
-        self::assertSame(null, Request::get('dzde'));
+        self::assertSame(null, $request->getValue('heyho'));
+        self::assertSame(null, $request->getValue('dzde'));
 
         $data = array(
             'dzde' => 'default',
             'heyho' => 'it exists'
         );
-        Request::init($data);
+        $request->init($data);
 
         // Now the data exists
-        self::assertSame('it exists', Request::get('heyho'));
-        self::assertSame('default', Request::get('dzde'));
+        self::assertSame('it exists', $request->getValue('heyho'));
+        self::assertSame('default', $request->getValue('dzde'));
 
         // Init with other data
         $data = array(
             'dzde' => 'wut?',
             'heyho' => 'yup, it\'s true'
         );
-        Request::init($data);
+        $request->init($data);
 
         // Now some other data exists
-        self::assertSame('yup, it\'s true', Request::get('heyho'));
-        self::assertSame('wut?', Request::get('dzde'));
+        self::assertSame('yup, it\'s true', $request->getValue('heyho'));
+        self::assertSame('wut?', $request->getValue('dzde'));
     }
 
     public static function testCanSetRequestValues()
     {
+        $request = new Request;
         // Init with other data
         $data = array(
             'section' => 'wut?',
         );
-        Request::init($data);
+        $request->init($data);
 
-        self::assertSame('wut?', Request::get('section'));
+        self::assertSame('wut?', $request->getValue('section'));
 
         // Set it to a new value
-        Request::set('section', 'whatever');
-        self::assertSame('whatever', Request::get('section'));
+        $request->setValue('section', 'whatever');
+        self::assertSame('whatever', $request->getValue('section'));
     }
 
     public function testCanPopulateFromGet()
     {
+        $request = new Request;
         // Just filling the superglobal...
         $_GET['hi'] = 'hey';
         self::assertArrayHasKey('hi', $_GET);
 
-        Request::populateFromGet();
+        $request->populateFromGet();
 
-        self::assertEquals('hey', Request::get('get-hi'));
+        self::assertEquals('hey', $request->getValue('get-hi'));
     }
 
     public function testCanPopulateFromPost()
     {
+        $request = new Request;
         // Just filling the superglobal...
         $_POST['hi'] = 'hey';
         self::assertArrayHasKey('hi', $_POST);
 
-        Request::populateFromPost();
+        $request->populateFromPost();
 
-        self::assertEquals('hey', Request::get('post-hi'));
+        self::assertEquals('hey', $request->getValue('post-hi'));
     }
 }
