e100ddd9893a77cc4d906cb1483980540f546fe9
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-10-26 23:49:46 +0200

    Updating hashes

--- src/constant_map.php
+++ src/constant_map.php
@@ -5,7 +5,7 @@
 
 return array(
    'css_hash' => 'f8f78e342e0b63b6e09348b577daa45865b88037',
-   'css_last_update' => '1382726112',
+   'css_last_update' => '1382822636',
    'js_hash' => '86ecefc353542f1d12a338f5229edaf1acb31027',
    'js_last_update' => '1382726119',
 );
