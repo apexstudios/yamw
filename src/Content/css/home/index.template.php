572ea6574a4352af3cb86f941c66fe5ec10bcae7
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-10-27 02:58:14 +0200

    Removing some unnecessary files

--- src/Content/css/home/index.template.php
+++
@@ -1,8 +0,0 @@
-<?php
-contenttype('text/css');
-cache();
-
-include path('css/mbExtruder.css');
-include path('css/jquery.notice.css');
-include path('css/apycom.menu.css');
-include path('css/main_src.css');
