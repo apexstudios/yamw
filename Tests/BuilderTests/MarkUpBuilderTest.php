3b446846f140a60607f09be7a38489f0248400da
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-11-10 15:24:38 +0100

    Fixed wrong case for UNIX systems

--- Tests/BuilderTests/MarkUpBuilderTest.php
+++ Tests/BuilderTests/MarkUpBuilderTest.php
@@ -1,21 +1,21 @@
 <?php
 use Yamw\Lib\Builders\Markup\HtmlTag;
 use Yamw\Lib\Builders\Markup\XmlTag;
-use Yamw\Lib\Builders\MarkupBuilder;
+use Yamw\Lib\Builders\MarkUpBuilder;
 use Yamw\Lib\Builders\Interfaces\YamwMarkup;
 
 class MarkUpBuilderTest extends PHPUnit_Framework_TestCase
 {
-    protected $class = '\\Yamw\\Lib\\Builders\\MarkupBuilder';
-    
+    protected $class = '\\Yamw\\Lib\\Builders\\MarkUpBuilder';
+
     public function testInstanceType()
     {
-        $this->assertInstanceOf($this->class, new MarkupBuilder());
+        $this->assertInstanceOf($this->class, new MarkUpBuilder());
     }
-    
+
     public function testSimpleBuild()
     {
-        $builder = new MarkupBuilder();
+        $builder = new MarkUpBuilder();
         $builder->addMarkUp(new XmlTag('a', 'This is a link', array('href' => 'http://megatokyo.com/')));
         $builder->addMarkUp(new HtmlTag('span', 'Or rather don\'t'));
         $this->assertRegExp("/<a href=\".*?\">.*?<\/a>\n.*?<span>.*?<\/span>/", $builder->retrieve());
